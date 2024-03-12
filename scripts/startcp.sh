4de8d972aa25dfb1cd3d8b7a455ba02ade50d342
AleSassi <AleSassi@users.noreply.github.com>
2024-03-12 11:07:36 +0000

    Fixed pinger pod and control plane starter script

--- scripts/startcp.sh
+++ scripts/startcp.sh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 # Copy the config files needed by workers to their volumes
-bash ./copyconfigs.sh
+bash /kind/copyconfigs.sh
 # Install CNI
 kubectl create --kubeconfig=/etc/kubernetes/admin.conf -f /kind/manifests/patched-cni.conf
 # Install storage class
